0bbc2e1f1b6e9350a96e5c2b51dec2ae585146ac
aayushharora <aayushh2k@gmail.com>
2024-02-10 20:31:10 +0530

    Frontpage

--- frontend/src/Components/Landing.jsx
+++ frontend/src/Components/Landing.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import twitterLogo from '../assets/twitter-logo.svg'
 
 export default function Landing() {
   return (
-    <div>Landing</div>
+    <div className="landing-parent flex">
+      <div className="landing-main flex">
+
+      <div className="landing-left flex">
+        <img src={twitterLogo} alt="" id="twitterLogo"/>
+      </div>
+
+      <div className="landing-right flex">
+        <div className="r-top flex">
+          <div className="top-heading">
+            Happening now
+          </div>
+          <div className="top-heading2">
+            Join today.
+          </div>
+        </div>
+        <div className="r-mid flex">
+          <div className="create-acc btn flex">
+            Create Account
+          </div>
+          <div className="terms">
+          By signing up, you agree to the <span id='terms-text'>Terms of Service</span> and <span id='terms-text'>Privacy Policy</span>, including <span id='terms-text'>Cookie Use.</span>
+          </div>
+        </div>
+        <div className="r-bottom flex">
+          <div className="existing-acc">
+            Already have an account?
+          </div>
+          <div className="sign-in btn flex">
+            Sign in
+          </div>
+        </div>
+      </div>
+      </div>
+
+      <footer>
+        <ul className='about flex'>
+          <li>About</li>
+          <li>Download the App</li>
+          <li>Help Centre</li>
+          <li>Terms of Service</li>
+          <li>Privacy Policy</li>
+          <li>Cookie Policy</li>
+          <li>Accessibility</li>
+          <li>Ads info</li>
+          <li>Blog</li>
+          <li>About</li>
+          <li>Status</li>
+          <li>Careers</li>
+          <li>Brand Resources</li>
+          <li>Advertising</li>
+          <li>Marketing</li>
+          <li>Twitter for Business</li>
+          <li>Developers</li>
+          <li>Directory</li>
+          <li id="by">by Rikhil & Aayush</li>
+        </ul>
+      </footer>
+      
+    </div>
   )
 }
