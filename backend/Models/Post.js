4bd6c14a8f571d58a66ba0c399ffecb412a3137b
aayushharora <aayushh2k@gmail.com>
2024-02-11 11:55:44 +0530

    Post Schema

---
+++ backend/Models/Post.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const formatDate = ()=> {
+    const date = new Date();
+    const options = { weekday: 'short', year: '2-digit', month: 'short', day: '2-digit', hour: 'numeric', minute: 'numeric' };
+    const formattedDate = date.toLocaleString('en-US', options);
+    return formattedDate;
+};
+
+function formatDate() {
+    const date = new Date();
+    const options = { 
+        timeZone: 'Asia/Kolkata',
+        weekday: 'short', 
+        year: '2-digit', 
+        month: 'short', 
+        day: '2-digit', 
+        hour: 'numeric', 
+        minute: 'numeric' 
+    };
+    const formattedDate = date.toLocaleString('en-IN', options);
+    return formattedDate;
+};
+
+
+const postSchema = new mongoose.Schema({
+    description:{
+        type: String,
+        required: true
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    likes:[{
+        type: mongoose.Schema.Types.ObjectId
+    }],
+    comments: [{
+        description: String,
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        date: {
+            type: String,
+            default: formatDate
+        }
+    }],
+    date: {
+        type: String,
+        default: formatDate
+    }
+});
+
+const Post = mongoose.model("Post",userSchema);
+module.exports = Post;
