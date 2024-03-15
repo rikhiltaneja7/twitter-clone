628af9523df61b38bf96f3d8512d4c25a2a7cc1c
aayushharora <aayush.arora@kalvium.community>
2024-03-15 16:54:26 +0530

    Card Component

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { AppContext } from './Context'
+import { Textarea, Avatar } from '@chakra-ui/react'
 import Post from './Post'
 import Posts from './Posts'
 
@@ -8,6 +9,13 @@ export default function Home() {
   return (
     <div className='home-parent'>
       <div className='username'>Hello, {currentUser}</div>
+      <div className="tweet-area flex">
+      <Avatar name={currentUser} src="https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png" />
+      <Textarea size ='lg' placeholder="What is Happening?!" resize={'none'} width={'30vh'}/>
+      <div className="btn post flex">
+        Post
+      </div>
+      </div>
       <Posts/>
     </div>
   )
--- frontend/src/Components/Post.jsx
+++ frontend/src/Components/Post.jsx
@@ -1,47 +1,51 @@
-import { Avatar, Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Text } from "@chakra-ui/react";
-import { BiLike, BiChat } from 'react-icons/bi';
+import { Avatar, Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { BiLike, BiChat, BiShare } from 'react-icons/bi';
 import React from "react";
 
 function Post({tweet}) {
   console.log(tweet);
   return (
     <div>
-      <Card maxW="xxl">
-        <CardHeader>
-          <Flex spacing="4">
-            <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
-              <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
+      <Card maxW='xl'>
+  <CardHeader>
+    <Flex spacing='4'>
+      <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+        <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
+        <Box>
+          <Heading size='sm'>{tweet.user}</Heading>
+          <Text>Creator, Chakra UI</Text>
+        </Box>
+      </Flex>
+    </Flex>
+  </CardHeader>
+  <CardBody>
+    <Text>
+      {tweet.description}
+    </Text>
+  </CardBody>
+  <Image
+    objectFit='cover'
+    src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+    alt='Chakra UI'
+  />
 
-              <Box>
-                <Heading size="sm">{tweet.user}</Heading>
-                <Text>Creator, Chakra UI</Text>
-              </Box>
-            </Flex>
-          </Flex>
-        </CardHeader>
-        <CardBody>
-          <Text>
-          {tweet.description}
-          </Text>
-        </CardBody>
-
-        <CardFooter
-          justify="space-between"
-          flexWrap="wrap"
-          sx={{
-            "& > button": {
-              minW: "136px",
-            },
-          }}
-        >
-          <Button flex="1" variant="ghost" leftIcon={<BiLike />}>
-            Like
-          </Button>
-          <Button flex="1" variant="ghost" leftIcon={<BiChat />}>
-            Comment
-          </Button>
-        </CardFooter>
-      </Card>
+  <CardFooter
+    justify='space-between'
+    flexWrap='wrap'
+    sx={{
+      '& > button': {
+        minW: '136px',
+      },
+    }}
+  >
+    <Button flex='1' variant='ghost' leftIcon={<BiLike />}>
+      Like
+    </Button>
+    <Button flex='1' variant='ghost' leftIcon={<BiChat />}>
+      Comment
+    </Button>
+  </CardFooter>
+</Card>
     </div>
   );
 }
